db57eb1ddb0cb152dab06157bd3576e4fecdebec
serdyuksb <serdiuk.sb@soctech-it.ru>
2022-09-09 14:26:25 +0300

    refactor: change stricture of the pom.xml, Lombok

--- src/main/java/ru/serdyuk/domain/BankTransaction.java
+++ src/main/java/ru/serdyuk/domain/BankTransaction.java
@@ -1,50 +1,15 @@
 package ru.serdyuk.domain;
 
+import lombok.*;
+
 import java.time.LocalDate;
 import java.util.Objects;
-
+@Data
+@Getter@Setter
+@EqualsAndHashCode
+@ToString
 public class BankTransaction {
     private final LocalDate date;
     public final double amount;
     public final String description;
-
-    public BankTransaction(LocalDate date, double amount, String description) {
-        this.date = date;
-        this.amount = amount;
-        this.description = description;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BankTransaction that = (BankTransaction) o;
-        return Double.compare(that.amount, amount) == 0 && date.equals(that.date) && description.equals(that.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(date, amount, description);
-    }
-
-    @Override
-    public String toString() {
-        return "BankTransaction{" +
-                "date=" + date +
-                ", amount=" + amount +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
