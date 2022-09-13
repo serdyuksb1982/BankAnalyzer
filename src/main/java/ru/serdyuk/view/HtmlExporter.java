7fedb344631ac7f3a5a2f499a92bca80ad75b834
serdyuksb <serdiuk.sb@soctech-it.ru>
2022-09-13 17:47:15 +0300

    feat: adding Exporter, HtmlExporter and SummaryStatistics

---
+++ src/main/java/ru/serdyuk/view/HtmlExporter.java
@@ -0,0 +1,23 @@
+package ru.serdyuk.view;
+
+import ru.serdyuk.domain.SummaryStatistics;
+import ru.serdyuk.services.interfaces.Exporter;
+
+public class HtmlExporter implements Exporter {
+    @Override
+    public String export(final SummaryStatistics summaryStatistics) {
+        String result = "<!doctype html>";
+        result += "<html lang='em'>";
+        result += "<head><title>Bank Transaction Report</title></head>";
+        result += "body";
+        result += "<ul>";
+        result += "<li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>";
+        result += "<li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>";
+        result += "<li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>";
+        result += "<li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>";
+        result += "</ul>";
+        result += "</body>";
+        result += "</html>";
+        return result;
+    }
+}
